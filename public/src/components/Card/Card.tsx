a880a33f19f06b395cf9630801a83b7a7fcb8e7a
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-12 17:31:43 -0400

    add Card component with styles and icon support

---
+++ public/src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import { type CardProps } from './Card.types';
+import { baseStyles, colorVariants, iconBgVariants } from './Card.styles';
+
+export const Card = ({ title, icon, color }: CardProps) => {
+  const iconPath = `../../../public/assets/images/${icon}.svg`;
+
+  return (
+    <div className={`${baseStyles.container} ${colorVariants[color] || colorVariants.blue}`}>
+      <div className={`${baseStyles.iconContainer} ${iconBgVariants[color] || iconBgVariants.blue}`}>
+        <img 
+          src={iconPath}
+          alt={`Icono de ${title}`} 
+          className={baseStyles.iconImage}
+        />
+      </div>
+      
+      <h3 className={baseStyles.title}>{title}</h3>
+    </div>
+  );
+};
