d1ff6abfafc571d306b398ac8a723b2ae25f7b62
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-12 17:32:03 -0400

    add getAreas.ts to define areas with titles, icons, and colors

---
+++ public/src/features/areas/api/getAreas.ts
@@ -0,0 +1,21 @@
+export const areas = [
+    {
+        id: "1",
+        title: "Mind",
+        icon: "mind",
+        color: "blue"
+    },
+    {
+        id: "2",
+        title: "Body",
+        icon: "body",
+        color: "green"
+    },
+    {
+        id: "3",
+        title: "Spirit",
+        icon: "spirit",
+        color: "purple"
+    }
+]
+
