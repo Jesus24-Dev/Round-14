e3df5f77721e9840a84af5fdda7ce38cf39b4132
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 11:25:29 -0400

    add(page): create HabitPage component to integrate AreaList and HabitsList

---
+++ public/src/pages/HabitPage.tsx
@@ -0,0 +1,11 @@
+import { AreaList } from "../features/areas/components/AreaList"
+import { HabitsList } from "../features/habits/components/HabitsList"
+
+export function HabitPage(){
+    return (
+    <div>
+      <AreaList />
+      <HabitsList />
+    </div>
+    )
+}
