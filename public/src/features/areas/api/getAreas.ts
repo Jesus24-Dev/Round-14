dc8193cdfa27b533ac7cffbaaac27057231068b1
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 10:35:26 -0400

    refactor(getAreas): replace hardcoded areas with dynamic import from data.json

--- public/src/features/areas/api/getAreas.ts
+++ public/src/features/areas/api/getAreas.ts
@@ -1,33 +1,7 @@
-export const areas = [
-    {
-        id: "1",
-        title: "Mind",
-        icon: "mind",
-        color: "blue"
-    },
-    {
-        id: "2",
-        title: "Body",
-        icon: "body",
-        color: "green"
-    },
-    {
-        id: "3",
-        title: "Spirit",
-        icon: "spirit",
-        color: "purple"
-    },
-    {
-        id: "4",
-        title: "Work",
-        icon: "work",
-        color: "orange"
-    },
-    {
-        id: "5",
-        title: "Relations",
-        icon: "relations",
-        color: "red"
-    }
-]
+import areas from '../../../data.json'
 
+const data = areas;
+
+export async function getAreas() {
+    return data;
+}
