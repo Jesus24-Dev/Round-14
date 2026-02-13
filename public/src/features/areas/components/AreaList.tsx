7fa92d25d5f0a4ac4e6eca87240cb7ce878a64d9
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 11:38:00 -0400

    fix(AreaList): add onClick handler to Card component for area selection

--- public/src/features/areas/components/AreaList.tsx
+++ public/src/features/areas/components/AreaList.tsx
@@ -1,14 +1,20 @@
 import { useAreas } from "../hooks/useAreas";
 import { Card } from "../../../components/Card";
+import { useHabit } from "../../../store/habits/useHabit";
 
 export function AreaList(){
     const { data, isLoading }  = useAreas();
+    const { updateAreaId } = useHabit();
+
+    const updateArea = (areaId: string) => {
+        updateAreaId(areaId);
+    }
     return(
         <>
             {isLoading ?? <p>loading...</p>}
             <div className="grid grid-cols-3 gap-4">
                 {data?.cards.map(card => (
-                    <Card key={card.id} id={card.id} title={card.title} icon={card.icon} color={card.color}/>
+                    <Card key={card.id} id={card.id} title={card.title} icon={card.icon} color={card.color} onClick={() => updateArea(card.id)}/>
                 ))}
             </div>
         </>
