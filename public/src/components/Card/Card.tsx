c130664c6794bf0d8ad7594a77d0e0291da0a610
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 11:37:50 -0400

    fix(Card): add onClick prop to Card component for click handling

--- public/src/components/Card/Card.tsx
+++ public/src/components/Card/Card.tsx
@@ -1,11 +1,11 @@
 import { type CardProps } from './Card.types';
 import { baseStyles, colorVariants, iconBgVariants } from './Card.styles';
 
-export const Card = ({ title, icon, color }: CardProps) => {
+export const Card = ({ title, icon, color, onClick }: CardProps) => {
   const iconPath = `../../assets/images/${icon}.svg`;
 
   return (
-    <div className={`${baseStyles.container} ${colorVariants[color] || colorVariants.blue}`}>
+    <div className={`${baseStyles.container} ${colorVariants[color] || colorVariants.blue}`} onClick={onClick}>
       <div className={`${baseStyles.iconContainer} ${iconBgVariants[color] || iconBgVariants.blue}`}>
         <img 
           src={iconPath}
