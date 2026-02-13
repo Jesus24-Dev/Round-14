f2ff9b16d082bd1c1af73be943ae1f2476513b3b
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 11:37:40 -0400

    add(provider): wrap App component with HabitProvider for context management

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { HabitProvider } from './store/habits/HabitProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HabitProvider>
+      <App />
+    </HabitProvider>
   </StrictMode>,
 )
