07a896a7b4e7fbcb3253367ac1c925818665d79f
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 10:23:35 -0400

    fix(AreaList): add key prop to Card component and improve layout structure

--- public/src/features/areas/components/AreaList.tsx
+++ public/src/features/areas/components/AreaList.tsx
@@ -3,13 +3,14 @@ import { Card } from "../../../components/Card";
 
 export function AreaList(){
     const { data, isLoading }  = useAreas();
-    console.log(data)
     return(
         <>
             {isLoading ?? <p>loading...</p>}
-            {data?.cards.map(card => (
-                <Card id={card.id} title={card.title} icon={card.icon} color={card.color}/>
-            ))}
+            <div className="grid grid-cols-3 gap-4">
+                {data?.cards.map(card => (
+                    <Card key={card.id} id={card.id} title={card.title} icon={card.icon} color={card.color}/>
+                ))}
+            </div>
         </>
     )
 }
