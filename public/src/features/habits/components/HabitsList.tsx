17cc3d8bea5c5e1ad150a0c03fd4a66a8409fd5d
jesus24-dev <siritjesus24@gmail.com>
2026-02-13 11:25:16 -0400

    add(component): create HabitsList component to display user habits

---
+++ public/src/features/habits/components/HabitsList.tsx
@@ -0,0 +1,18 @@
+import { useHabits } from "../hooks/useHabits";
+import { type Habits } from "../types/Habits";
+
+export function HabitsList(){
+    const {habits} = useHabits('1')
+
+    return (
+        <div>
+            {habits?.map((habit: Habits) => (
+                <div key={habit.id}>
+                    <h3>{habit.title}</h3>
+                    <p>{habit.description}</p>
+                    <p>{habit.frequency}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
