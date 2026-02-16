5b146389aff44bb25b9a4be06845add01e6f1bf0
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 11:51:17 -0400

    fix(WeightControlPage): Add components for the page. Add logic for modal.

--- public/src/pages/WeightControlPage.tsx
+++ public/src/pages/WeightControlPage.tsx
@@ -1,7 +1,30 @@
+import {useState} from 'react'
+import { Button } from "../components/Button";
+import { ModalForm } from "../features/modalforms/components/ModalForm";
 import { WeightList } from "../features/weights/components/WeightList";
+import { type InputProps } from "../components/Input";
+
+const weightFormInputs: InputProps[] =[
+    { name: "weight", type: "number" },
+    { name: "date", type: "date" },
+    { name: "hoursOfSleep", type: "number"},
+    { name: "waterIntake", type: "number"}
+]
 
 export function WeightControlPage(){
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpenModal = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
-        <WeightList />
+        <div className="container mx-auto p-4">
+            <WeightList />    
+            <Button text="Add Weight" color="primary" type="button" onClick={handleOpenModal} />
+            <ModalForm inputs={weightFormInputs} action="/api/weights" method="POST" title="Add New Weight" isOpen={isOpen} onClose={handleOpenModal}/>
+        </div>
+           
     )
 }
