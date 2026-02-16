63f266db5544f6aec67a87e8497991c2013b9226
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 09:46:35 -0400

    feat: add PageProvider

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { HabitProvider } from './store/habits/HabitProvider.tsx'
+import { PageProvider } from './store/pages/PageProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HabitProvider>
-      <App />
-    </HabitProvider>
+    <PageProvider>
+      <HabitProvider>
+        <App />
+      </HabitProvider>
+    </PageProvider>
   </StrictMode>,
 )
