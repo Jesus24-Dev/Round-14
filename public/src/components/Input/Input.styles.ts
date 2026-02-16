895a26ff6085d7a475277c40f4f3e6154b20026f
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 10:22:42 -0400

    feat(Input) add base styles for Input

---
+++ public/src/components/Input/Input.styles.ts
@@ -0,0 +1,13 @@
+export const baseStyle = {
+    display: 'block',
+    width: '100%',
+    padding: '0.375rem 0.75rem',
+    fontSize: '1rem',
+    lineHeight: 1.5,
+    color: '#495057',
+    backgroundColor: '#fff',
+    backgroundClip: 'padding-box',
+    border: '1px solid #ced4da',
+    borderRadius: '0.25rem',
+    transition: 'border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out',
+}
