2a203c98a8d20d1711e279589db1970ad0a4ff10
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 09:45:54 -0400

    feat: add structure for the main page. add conditional rendering with global context.

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,12 +1,24 @@
 import { HabitPage } from "./pages/HabitPage"
 import { WeightControlPage } from "./pages/WeightControlPage"
+import { Sidebar } from "./features/sidebar/components/Sidebar"
+import { HomePage } from "./pages/HomePage"
+import { usePage } from "./store/pages/usePage"
 
 function App() {
+
+  const {page} = usePage();
+
   return (
-    <>
-      <HabitPage />
-      <WeightControlPage />
-    </>
+    <div className="grid grid-cols-5">
+      <div className="col-span-1">
+        <Sidebar />
+      </div>
+      <div className="col-span-4 col-start-2 p-8">
+        {page === "home" && <HomePage />}
+        {page === "habits" && <HabitPage />}
+        {page === "weight" && <WeightControlPage />}
+      </div>
+    </div>
   )
 }
 
