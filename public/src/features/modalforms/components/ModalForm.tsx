ae0d0b823dc09dfc50648231a6b9e65e4b93f2d1
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 11:50:01 -0400

    feat(ModalForm): created component with Modal, form, Input and button.

---
+++ public/src/features/modalforms/components/ModalForm.tsx
@@ -0,0 +1,19 @@
+import { type ModalFormProps } from "../types/ModalForm";
+import { Input } from "../../../components/Input";
+import { Modal, type ModalProps } from "../../../components/Modal";
+import { Button } from "../../../components/Button";
+
+type Props = ModalFormProps & Omit<ModalProps, "children">;
+
+export function ModalForm({inputs, action, method, ...modalProps}: Props) {
+    return (
+        <Modal {...modalProps}>
+            <form action={action} method={method}>
+                {inputs.map((input, index) => (
+                    <Input key={index} {...input} />
+                ))}
+                <Button text="Submit" color="primary" type="submit"/>
+            </form>
+        </Modal>
+    )
+}
