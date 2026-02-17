905b748d5339b859e3424efaac657aa8f798f082
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-17 12:27:18 -0400

    delete(HabitPage): deleted unused HabitList component.

--- public/src/pages/HabitPage.tsx
+++ public/src/pages/HabitPage.tsx
@@ -1,11 +1,9 @@
 import { AreaList } from "../features/areas/components/AreaList"
-import { HabitsList } from "../features/habits/components/HabitsList"
 
 export function HabitPage(){
     return (
     <div>
       <AreaList />
-      <HabitsList />
     </div>
     )
 }
