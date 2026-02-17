afc1be6b54e638fac8e8b8e1270b4a5f24c30de8
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-17 12:41:39 -0400

    delete(PageContext): delete unused page context.

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -3,14 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
-import { PageProvider } from './store/pages/PageProvider.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <PageProvider>
-          <App />
-      </PageProvider>
+        <App />
     </BrowserRouter>
   </StrictMode>,
 )
--- public/src/store/pages/PageContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-interface PageContextType {
-    page: string;
-    updatePage: (newPage: string) => void;
-}
-
-export const PageContext = createContext<PageContextType | undefined>(undefined)
