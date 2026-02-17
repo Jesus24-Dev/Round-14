dff6e70d478338e1d6ee847b34c158d186f7b39e
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-17 12:36:55 -0400

    refactor(App): add Routes with React Router for each page.

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,24 +1,19 @@
+import {Routes, Route} from "react-router-dom"
 import { HabitPage } from "./pages/HabitPage"
 import { WeightControlPage } from "./pages/WeightControlPage"
-import { Sidebar } from "./features/sidebar/components/Sidebar"
 import { HomePage } from "./pages/HomePage"
-import { usePage } from "./store/pages/usePage"
+import { HabitDetailPage } from "./pages/HabitDetailPage"
+
 
 function App() {
-
-  const {page} = usePage();
-
   return (
-    <div className="grid grid-cols-5">
-      <div className="col-span-1">
-        <Sidebar />
-      </div>
-      <div className="col-span-4 col-start-2 p-8">
-        {page === "home" && <HomePage />}
-        {page === "habits" && <HabitPage />}
-        {page === "weight" && <WeightControlPage />}
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/habits" element={<HabitPage />} />
+      <Route path="/weight-control" element={<WeightControlPage />} />
+      <Route path="/habit-detail/:areaId" element={<HabitDetailPage />} />
+    </Routes>
+
   )
 }
 
