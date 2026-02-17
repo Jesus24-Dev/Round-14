c7e1c55405de49502483dac4ae134eecf2c04388
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 22:48:02 -0400

    feat(main): Add BrowserRouter to use React Router. Delete HabitProvider.

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import { HabitProvider } from './store/habits/HabitProvider.tsx'
+
 import { PageProvider } from './store/pages/PageProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PageProvider>
-      <HabitProvider>
-        <App />
-      </HabitProvider>
-    </PageProvider>
+    <BrowserRouter>
+      <PageProvider>
+          <App />
+      </PageProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
