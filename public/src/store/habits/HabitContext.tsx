139b760785a9d21bff9deee75548eedd37145370
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-16 22:44:34 -0400

    delete(HabitContext): delete global context for habits. Use react router instead of.

--- public/src/store/habits/HabitContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-interface HabitContextType {
-    areaId: string;
-    updateAreaId: (newAreaId: string) => void;
-}
-
-export const HabitContext = createContext<HabitContextType | undefined>(undefined)
