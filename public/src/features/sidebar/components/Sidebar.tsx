2f6d189ef20be0cf34ce34d9f5a4314eeba39987
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-17 12:35:51 -0400

    refactor(Sidebar): change the urls correctly.

--- public/src/features/sidebar/components/Sidebar.tsx
+++ public/src/features/sidebar/components/Sidebar.tsx
@@ -3,9 +3,9 @@ import { SidebarLink } from "../../../components/SidebarLink"
 export function Sidebar(){
     return (
         <div className="w-64 bg-white shadow-md h-screen p-4">
-            <SidebarLink to="home" label="Home" />
-            <SidebarLink to="habits" label="Habits" />
-            <SidebarLink to="weight" label="Weight Control" />
+            <SidebarLink to="/" label="Home" />
+            <SidebarLink to="/habits" label="Habits" />
+            <SidebarLink to="/weight-control" label="Weight Control" />
         </div>
     )
 }
