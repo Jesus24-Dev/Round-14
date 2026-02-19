02cffa7b99f08b3b584f54d5bd33f83b06524e78
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-19 11:30:44 -0400

    feat(HabitDetailPage): send data to the api with FETCH.

--- public/src/pages/HabitDetailPage.tsx
+++ public/src/pages/HabitDetailPage.tsx
@@ -7,14 +7,15 @@ import { type InputProps } from "../components/Input";
 import { Grid } from '../components/Grid/Grid';
 import { Sidebar } from '../features/sidebar/components/Sidebar';
 
-const habitFormInputs: InputProps[] =[
-    { name: "title", type: "string" },
-    { name: "description", type: "string" },
-    { name: "frequency", type: "string"},
+const habitFormInputs: InputProps[] = [
+    { name: "title", type: "text", placeholder: "Enter habit title" },
+    { name: "description", type: "text", placeholder: "Enter habit description" },
+    { name: "frequency", type: "text", placeholder: "Enter habit frequency" },
 ]
 
 export function HabitDetailPage(){
     const { areaId } = useParams();
+    habitFormInputs.push({ name: "areaId", type: "hidden", value: areaId})
 
     const [isOpen, setIsOpen] = useState(false)
 
@@ -28,6 +29,31 @@ export function HabitDetailPage(){
         navigate('/habits')
     }
 
+        const handleSubmit = async (event: React.SubmitEvent) => {
+            event.preventDefault()
+            const formData = new FormData(event.target)
+            const form = event.target;
+            const data = Object.fromEntries(formData.entries())
+            console.log(data)
+
+            try {
+                const response = await fetch("http://localhost:3000/api/habits", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if (response.ok){
+                    console.log("habits data submitted successfully")    
+                }
+                form.reset();
+            } catch (error){
+                console.error("Error submitting habits data:", error)
+            }
+        }
+
     return (
         <Grid sidebar={<Sidebar />} content={
             <div>
@@ -35,7 +61,7 @@ export function HabitDetailPage(){
             {areaId ? <HabitsList areaId={areaId} /> : <p>No area ID provided.</p>}
             <Button text="Add Habit" color="primary" type="button" onClick={handleOpenModal} />
             <Button text="Go Back" color="secondary" type="button" onClick={goBack} />
-            <ModalForm inputs={habitFormInputs} action="/api/habits" method="POST" title="Add New Habit" isOpen={isOpen} onClose={handleOpenModal}/>    
+            <ModalForm inputs={habitFormInputs} title="Add New Habit" isOpen={isOpen} onClose={handleOpenModal} onSubmit={handleSubmit}/>    
         </div>
         }/>
 
