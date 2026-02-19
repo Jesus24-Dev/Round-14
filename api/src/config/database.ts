8c2e4cf32fc731cc9135d3d829f622f24e7023a8
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-19 09:54:10 -0400

    feat(database): add initial config for database connection.

---
+++ api/src/config/database.ts
@@ -0,0 +1,9 @@
+export const config = {
+    database: {
+        host: "localhost",
+        user: "postgres",
+        database: "round14",
+        password: "123456",
+        port: 5432
+    }
+}
