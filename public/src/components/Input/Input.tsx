1ec50dfecfbc540afc026f083758c6d97d1e683a
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-19 11:28:38 -0400

    feat(Input): add value optional propertie

--- public/src/components/Input/Input.tsx
+++ public/src/components/Input/Input.tsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { type InputProps } from "./Input.types";
 import { baseStyle } from "./Input.styles";
 
-export function Input({name, type, placeholder, disabled}: InputProps){
-    const [value, setValue] = useState("");
+export function Input({name, type, placeholder, disabled, value}: InputProps){
+    const [inputValue, setInputValue] = useState(value || "");
 
     const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value)
+        setInputValue(e.target.value)
     }
 
     return (
@@ -16,7 +16,7 @@ export function Input({name, type, placeholder, disabled}: InputProps){
             placeholder={placeholder}
             disabled={disabled}
             style={baseStyle}
-            value={value}
+            value={inputValue}
             onChange={handleInputValue}
         />
     )
--- public/src/components/Input/Input.types.ts
+++ public/src/components/Input/Input.types.ts
@@ -1,6 +1,7 @@
 export interface InputProps {
     name: string;
     type: string;
+    value?: string;
     placeholder?: string;
     disabled?: boolean;
 }
