ba1745959fb6e2a4b841f93dafda9ad7985b856a
Jesus24-Dev <siritjesus24@gmail.com>
2026-02-19 10:51:11 -0400

    feat(WeightControlPage): Add POST request to send data.  Add placeholders for inputs.

--- public/src/pages/WeightControlPage.tsx
+++ public/src/pages/WeightControlPage.tsx
@@ -7,10 +7,11 @@ import { Grid } from "../components/Grid/Grid"
 import { Sidebar } from "../features/sidebar/components/Sidebar"
 
 const weightFormInputs: InputProps[] =[
-    { name: "weight", type: "number" },
+    { name: "weight", type: "number", placeholder: "Weight in kg"},
     { name: "date", type: "date" },
-    { name: "hoursOfSleep", type: "number"},
-    { name: "waterIntake", type: "number"}
+    { name: "hoursOfSleep", type: "number", placeholder: "Hours of sleep"},
+    { name: "energy", type: "number", placeholder: "Energy level (1-10)"},
+    { name: "waterIntake", type: "number", placeholder: "Water intake in liters"}
 ]
 
 export function WeightControlPage(){
@@ -21,12 +22,36 @@ export function WeightControlPage(){
         setIsOpen(!isOpen)
     }
 
+    const handleSubmit = async (event: React.SubmitEvent) => {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        const form = event.target;
+        const data = Object.fromEntries(formData.entries())
+
+        try {
+            const response = await fetch("http://localhost:3000/api/weights", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok){
+                console.log("Weight data submitted successfully")    
+            }
+            form.reset();
+        } catch (error){
+            console.error("Error submitting weight data:", error)
+        }
+    }
+
     return (
         <Grid sidebar={<Sidebar />} content={
             <div className="container mx-auto p-4">
                 <WeightList />    
                 <Button text="Add Weight" color="primary" type="button" onClick={handleOpenModal} />
-                <ModalForm inputs={weightFormInputs} action="/api/weights" method="POST" title="Add New Weight" isOpen={isOpen} onClose={handleOpenModal}/>
+                <ModalForm inputs={weightFormInputs} title="Add New Weight" isOpen={isOpen} onClose={handleOpenModal} onSubmit={handleSubmit}/>
             </div>
         }/>
            
